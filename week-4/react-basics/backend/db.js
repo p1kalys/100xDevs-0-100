5461346f2ceaf3b15cc13240b2b8bf4312647774
p1kalys <pavanemani14@gmail.com>
2024-01-30 00:25:41 +0530

    Update

--- week-4/react-basics/backend/db.js
+++ week-4/react-basics/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://gibs:1234567890@cluster0.gyiz5tw.mongodb.net/');
+mongoose.connect('URL');
 
 const todoSchema = new mongoose.Schema({
   title: String,
